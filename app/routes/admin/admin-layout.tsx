3b6e806385d1cf0cc23f907876bcae7505c58096
Ali Shaikh <shahinshaikh603@gmail.com>
2025-04-25 23:29:06 +0530

    feat: add initial routing setup

---
+++ app/routes/admin/admin-layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Outlet} from "react-router";
+
+const AdminLayout = () => {
+    return (
+        <div className="admin-layout">
+            Mobile Layout
+            <aside className="w-full max-w-[270px] hidden lg-block">
+                Side Bar
+            </aside>
+            <aside className="children">
+                <Outlet/>
+            </aside>
+        </div>
+    )
+}
+export default AdminLayout
